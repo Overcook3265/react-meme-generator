c213bef3153f688fb9a0b807d627e58c53a10361
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-16 22:09:20 +0100

    Add basic setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function App() {
+  return (
+    <>
+      <div>Hello Darkness my old friend</div>
+      <br />
+      <div>
+        <img
+          src="https://api.memegen.link/images/doge/much_meme/high_quality.png"
+          alt="Meme test"
+        />
+      </div>
+    </>
+  );
+}
---
+++ src/MemeInput.js
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+
+export default function MemeInput() {
+  const [upperText, setUpperText] = useState('');
+  const [name, setName] = useState('');
+}
