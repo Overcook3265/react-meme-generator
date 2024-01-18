911b56b853450f6a08bb8d2f53c81630776e98ad
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-18 14:27:55 +0100

    Add download functionality using file-saver, remove input value attribute

--- src/MemeInput.js
+++ src/MemeInput.js
@@ -1,46 +1,38 @@
+import { saveAs } from 'file-saver';
 // import fs from 'node:fs';
-// import https from 'node:https';
+// import http from 'node:http';
 import { useState } from 'react';
 
 // declaration of variables
 export default function MemeInput() {
   const [memeName, setMemeName] = useState('doge');
-  const [upperText, setUpperText] = useState('');
+  const [upperText, setUpperText] = useState('_');
   const [lowerText, setLowerText] = useState('');
+  const defaultPic = `https://api.memegen.link/images/${memeName}/_/_.png`;
+  const memePic = `https://api.memegen.link/images/${memeName}/${upperText}/${lowerText}.png`;
+
   //  const [imgAddress, setImgAddress] = useState(
   //     `https://api.memegen.link/images/doge/_/_.png`,
   //   );
 
-  // function downloadMeme() {
-  //   const stream = fs.createWriteStream(
-  //     `./memes/${memeName}_${upperText}_${lowerText}.jpg`,
-  //   );
-  //   https.get(
-  //     `https://api.memegen.link/images/${memeName}/${upperText}/${lowerText}.png`,
-  //     function (response) {
-  //       response.pipe(stream);
-  //     },
-  //   );
+  // function downloadFile(url, fileName) {
+  //   fetch(url, {
+  //     method: 'get',
+  //     mode: 'no-cors',
+  //     referrerPolicy: 'no-referrer',
+  //   })
+  //     .then((res) => res.blob())
+  //     .then((res) => {
+  //       const aElement = document.createElement('a');
+  //       aElement.setAttribute('download', fileName);
+  //       const href = URL.createObjectURL(res);
+  //       aElement.href = href;
+  //       aElement.setAttribute('target', '_blank');
+  //       aElement.click();
+  //       URL.revokeObjectURL(href);
+  //     })
+  //     .catch((err) => alert(err));
   // }
-
-  function downloadFile(url, fileName) {
-    fetch(url, {
-      method: 'get',
-      mode: 'no-cors',
-      referrerPolicy: 'no-referrer',
-    })
-      .then((res) => res.blob())
-      .then((res) => {
-        const aElement = document.createElement('a');
-        aElement.setAttribute('download', fileName);
-        const href = URL.createObjectURL(res);
-        aElement.href = href;
-        aElement.setAttribute('target', '_blank');
-        aElement.click();
-        URL.revokeObjectURL(href);
-      })
-      .catch((err) => alert(err));
-  }
 
   return (
     <>
@@ -49,7 +41,8 @@ export default function MemeInput() {
           Meme Template <br />
           <input
             // id="memeName"
-            value={memeName}
+            // value={memeName}
+            placeholder="Name the Meme"
             onChange={(event) => {
               setMemeName(event.currentTarget.value);
             }}
@@ -61,7 +54,8 @@ export default function MemeInput() {
           Top Text <br />
           <input
             // id="upperText"
-            value={upperText}
+            // value={upperText}
+            placeholder="Top Text"
             onChange={(event) => {
               setUpperText(event.currentTarget.value);
             }}
@@ -72,26 +66,35 @@ export default function MemeInput() {
           Bottom Text <br />
           <input
             // id="lowerText"
-            value={lowerText}
+            // value={lowerText}
+            placeholder="Bottom Text"
             onChange={(event) => {
               setLowerText(event.currentTarget.value);
             }}
           />
         </label>
       </form>
-      <button onClick={downloadFile}>Download</button>
+      {/* <button onClick={downloadMeme}>Download</button> */}
+      <button
+        onClick={function () {
+          saveAs(memePic, `${memeName}_${upperText}_${lowerText}.jpg`);
+        }}
+      >
+        Download
+      </button>
       <br />
+      {/* <div>
+        <img
+          src={`https://api.memegen.link/images/doge/${upperText}/${lowerText}.png`}
+          alt="Meme test"
+        />
+      </div> */}
+
       <div id="meme">
-        {upperText === '' ? (
-          <img
-            src={`https://api.memegen.link/images/${memeName}/_/_.png`}
-            alt="Meme test"
-          />
+        {upperText === '' || memeName === '' ? (
+          <img src={defaultPic} alt="Meme test" />
         ) : (
-          <img
-            src={`https://api.memegen.link/images/${memeName}/${upperText}/${lowerText}.png`}
-            alt="Meme test"
-          />
+          <img src={memePic} alt="Generated Meme" />
         )}
       </div>
     </>
