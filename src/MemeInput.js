dd4b642b364cfbed0acb75bcd9d929d3edde0c40
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-19 17:45:35 +0100

    Add more CSS

--- src/MemeInput.js
+++ src/MemeInput.js
@@ -20,6 +20,11 @@ export default function MemeInput() {
       <div className={styles.background}>
         <div className={styles.frame}>
           <form className={styles.form}>
+            <h4>
+              ! FRESH MEMES !
+              <br /> first come first serve
+              <br />( ͡° ͜ʖ ͡°)
+            </h4>
             {/* <label>
           Meme template <br />
           <input
@@ -85,7 +90,7 @@ export default function MemeInput() {
             {upperText === '' || memeName === '' ? (
               <img
                 src={defaultPic}
-                alt="Meme test"
+                alt="Where's the meme? ಠ_ಠ"
                 data-test-id="meme-image"
                 className={styles.picture}
               />
